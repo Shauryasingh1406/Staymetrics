b6893c901364a42769e38b5e83e358f34d06839e
Shaurya pratap singh rajawat <92384194+Shauryasingh1406@users.noreply.github.com>
2026-08-09 23:51:56 +0000

    Bronze_layer done

---
+++ Staymetrics_data_pipeline/analyses/if_else.sql
@@ -0,0 +1,12 @@
+{% set flag=1 %}
+
+
+
+select * from {{ref('bronze_bookings')}}
+{% if flag == 1 %}
+    where NIGHTS_BOOKED >1
+{% else %}
+    where NIGHTS_BOOKED =1 
+{% endif %}
+
+
--- Staymetrics_data_pipeline/models/example/sources/Bronze/bronze_bookings.sql
+++ Staymetrics_data_pipeline/models/example/sources/Bronze/bronze_bookings.sql
@@ -1,5 +1,18 @@
 {{ config(
-    schema='bronze'
+    materialized='incremental',
+    schema='bronze',
+    incremental_strategy='merge'
 ) }}
 
-select * from {{source('STAGING','BOOKINGS')}}
+SELECT *
+FROM {{ source('STAGING', 'BOOKINGS') }}
+
+{% if is_incremental() %}
+    WHERE CREATED_AT > (
+        SELECT COALESCE(
+            MAX(CREATED_AT),
+            '1900-01-01'
+        )
+        FROM {{ this }}
+    )
+{% endif %}
